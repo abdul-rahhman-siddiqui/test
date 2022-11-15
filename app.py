6a40c6b63dfea5d7f4bf1c6068ab0747e988d24a
abdul-rahhman-siddiqui <60566862+abdul-rahhman-siddiqui@users.noreply.github.com>
2022-11-15 16:37:31 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request, send_file
+import io
+import torch
+import  gc
+# from torch import autocast 
+from torch.cuda.amp import autocast
+
 
 app = Flask(__name__)
-
+def run_inference(prompt):
+  return prompt
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route('/results', methods= ["POST"])
+def submit():
+    if request.method == 'POST':
+        prompt = request.form.get("Prompt")
+        img_data = run_inference(prompt)
+    return render_template("results.html",n=img_data)
